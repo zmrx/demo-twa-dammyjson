3d6b2a9fa7ce4873611588bbccd8590935ba9b11
zmrx <zmrxqq@gmail.com>
2025-12-23 01:17:14 +0500

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/hints',
+    '@nuxt/image'
+  ]
+})
